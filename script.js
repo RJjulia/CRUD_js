59a18df741a6cf618fe7fc35ff9439cee4f3af80
RJjulia <juliazzkx@gmail.com>
2023-02-20 22:52:50 -0300

    crud

---
+++ script.js
@@ -0,0 +1,11 @@
+const modal = document.querySelector('.modal-container')
+const tabBody = document.querySelector('tabBody')
+const sNome = document.querySelector('#m-nome')
+const sIdade = document.querySelector('#m-idade')
+const sTutor = document.querySelector('#m-tutor')
+const sContato = document.querySelector('#m-contato')
+const btnSalvar = document.querySelector('#btnSalvar')
+
+let itens
+let id
+
